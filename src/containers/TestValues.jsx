ca3f2544227d5ff8e4618cb73d5c38bc4df6cb82
Arturo0911 <anegreiross@outlook.com>
2021-08-12 12:46:18 -0500

    refactoring

--- src/containers/TestValues.jsx
+++ src/containers/TestValues.jsx
@@ -1,9 +1,12 @@
-import React, {useState} from 'react';
-import {connect} from 'react-redux';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 // import { bacteriaRequest} from '../actions';
 
 
-
+/**
+ * 
+ * [minimum_milk_proteins, titratable_acidity, pH_milk_sour_, fat_milk_over_100mg_
+ */
 
 const TestValues = props => {
 
@@ -13,48 +16,48 @@ const TestValues = props => {
     });
 
 
-    const handleInput = event =>{
+    const handleInput = event => {
         Setvalues({
             ...form,
-            [event.target.name]:event.target.value 
+            [event.target.name]: event.target.value
         })
     }
 
-    const handleSubmit = event =>{
+    const handleSubmit = event => {
         event.preventDefault();
         // props.bacteriaRequest(form);
         console.log(form);
         props.history.push("/response")
-    }   
+    }
 
 
     return (
         <div className="container p-4">
-            <h1 className="card-text"> Prediccion bacteriana.</h1>
+            <h1 className="card-text">Simulación de predicción.</h1>
             <form onSubmit={handleSubmit} encType="multipart/form-data">
                 <div className="row">
                     <div className="form-group col-md-6">
                         <div className="form-floating mb-3">
-                            <input 
-                                name="idealTemperature"
-                                type="text" 
-                                className="form-control" 
-                                id="floatingInput" 
-                                placeholder="Temperatura ideal"
+                            <input
+                                name="targetBacterian"
+                                type="number"
+                                className="form-control"
+                                id="floatingInput"
+                                placeholder="Predicción bacteriana"
                                 onChange={handleInput}
                             />
-                            <label htmlFor="floatingInput">Temperatura ideal</label>
+                            <label htmlFor="floatingInput">Predicción bacteriana.</label>
                         </div>
                     </div>
                     <div className="form-group col-md-6">
                         <div className="form-floating mb-3">
-                            <input 
-                            name="minProteins"
-                            type="text" 
-                            className="form-control" 
-                            id="floatingInput" 
-                            placeholder="Proteínas mínimas"
-                            onChange={handleInput}
+                            <input
+                                name="minProteins"
+                                type="number"
+                                className="form-control"
+                                id="floatingInput"
+                                placeholder="Proteínas mínimas"
+                                onChange={handleInput}
                             />
                             <label htmlFor="floatingInput">Proteínas mínimas</label>
                         </div>
@@ -62,12 +65,12 @@ const TestValues = props => {
                     <div className="form-group col-md-6">
                         <div className="form-floating mb-3">
                             <input
-                            name="tritatableAcid"
-                            type="text" 
-                            className="form-control" 
-                            id="floatingInput" 
-                            placeholder="Ácido tritatable"
-                            onChange={handleInput}
+                                name="tritatableAcid"
+                                type="number"
+                                className="form-control"
+                                id="floatingInput"
+                                placeholder="Ácido tritatable"
+                                onChange={handleInput}
                             />
                             <label htmlFor="floatingInput">Ácido tritatable</label>
                         </div>
@@ -75,12 +78,12 @@ const TestValues = props => {
                     <div className="form-group col-md-6">
                         <div className="form-floating mb-3">
                             <input
-                            name="phSour"
-                            type="text" 
-                            className="form-control" 
-                            id="floatingInput" 
-                            placeholder="pH suero de leche"
-                            onChange={handleInput}
+                                name="phSour"
+                                type="number"
+                                className="form-control"
+                                id="floatingInput"
+                                placeholder="pH suero de leche"
+                                onChange={handleInput}
                             />
                             <label htmlFor="floatingInput">pH suero de leche</label>
                         </div>
@@ -88,12 +91,12 @@ const TestValues = props => {
                     <div className="form-group col-md-6">
                         <div className="form-floating mb-3">
                             <input
-                            name="fatMilk"
-                            type="text" 
-                            className="form-control" 
-                            id="floatingInput" 
-                            placeholder="Grasa de la leche sobre 100mg"
-                            onChange={handleInput}
+                                name="fatMilk"
+                                type="number"
+                                className="form-control"
+                                id="floatingInput"
+                                placeholder="Grasa de la leche sobre 100mg"
+                                onChange={handleInput}
                             />
                             <label htmlFor="floatingInput">Grasa de la leche sobre 100mg</label>
                         </div>
@@ -104,6 +107,34 @@ const TestValues = props => {
                 </div>
 
             </form>
+
+            {/* <div className="jumbotron">
+                <h1 className="display-4"></h1>
+                <p className="lead"></p>
+                <hr className="my-4" />
+                <p></p>
+                <a className="btn btn-primary btn-log" href=""></a>
+            </div> */}
+            <div className="card text-center">
+                <div className="card-header">
+                    <ul className="nav nav-tabs card-header-tabs">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="true" href="#">Active</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="card-body">
+                    <h5 className="card-title">Special title treatment</h5>
+                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
         </div>
 
 
