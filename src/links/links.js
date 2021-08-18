e75de212ee016a00a50318b8d1f90a138a386696
Arturo0911 <anegreiross@outlook.com>
2021-08-17 23:47:22 -0500

    fixed connnection

--- src/links/links.js
+++ src/links/links.js
@@ -2,7 +2,7 @@
 /**
  * @description: losses, accuracy from classification network first microservice
  */
-export const APIFLASKM1 = "http://127.0.0.1:5000/charting";
+export const APIFLASKM1 = "http://0.0.0.0:5000/charting";
 
 /**
  * @description: growth log and time from the second microservices
@@ -13,34 +13,34 @@ export const APIFlask = "http://0.0.0.0:4000/bacteria";
 /**
  * @description: Streptococcus prediction first microservice.
  */
-export const APISingleStrep = "http://127.0.0.1:5000/strep";
+export const APISingleStrep = "http://0.0.0.0:5000/strep";
 
 /**
  * @description: Streptococcus Multiple predictions first microservice.
  */
-export const APIMultipleStrep = "http://172.17.0.2:5000/list_strep";
+export const APIMultipleStrep = "http://0.0.0.0:5000/list_strep";
 
 /**
  * @description: Lactobacillus prediction first microservice.
  */
-export const APISinlgeLact = "http://127.0.0.1:5000/lact";
+export const APISinlgeLact = "http://0.0.0.0:5000/lact";
 
 /**
  * @description: Lactobacillus Multiple predictions first microservice.
  */
-export const APIMultipleLact = "http://172.17.0.2:5000/list_lact";
+export const APIMultipleLact = "http://0.0.0.0:5000/list_lact";
 
 
 
 /**
  * @description: Classification model endpoint
  */
-export const APISingleClass = "http://127.0.0.1:5000/classification_single";
+export const APISingleClass = "http://0.0.0.0:5000/classification_single";
 
 
 /**
  * @description: Classification model endpoint
  */
- export const APIMultipleClass = "http://127.0.0.1:5000/classification_multiple";
+ export const APIMultipleClass = "http://0.0.0.0:5000/classification_multiple";
 
  
